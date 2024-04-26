0d959aa8285078e4b95c732cf4082d9bd43d5fce
Emredinho61 <emre-samet61@hotmail.de>
2024-04-26 22:52:08 +0200

    added Quasar as UI-lib

---
+++ Frontend/PokemonReviewAppFrontend/src/components/CarouselOptions.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="q-pa-md">
+        <div class="q-gutter-md">
+            <q-carousel v-model="slide" transition-prev="scale" transition-next="scale" swipeable animated
+                control-color="blue" navigation padding arrows height="600px" class="shadow-1 rounded-borders">
+                <q-carousel-slide name="style" class="column no-wrap flex-center">
+                    <SeeResults></SeeResults>
+                </q-carousel-slide>
+                <q-carousel-slide name="tv" class="column no-wrap flex-center">
+                    <q-icon name="live_tv" size="56px" />
+                    <div class="q-mt-md text-center">
+                        {{ lorem }}
+                    </div>
+                </q-carousel-slide>
+                <q-carousel-slide name="layers" class="column no-wrap flex-center">
+                    <q-icon name="layers" size="56px" />
+                    <div class="q-mt-md text-center">
+                        {{ lorem }}
+                    </div>
+                </q-carousel-slide>
+                <q-carousel-slide name="map" class="column no-wrap flex-center">
+                    <q-icon name="terrain" size="56px" />
+                    <div class="q-mt-md text-center">
+                        {{ lorem }}
+                    </div>
+                </q-carousel-slide>
+            </q-carousel>
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import SeeResults from 'components/SeeResults.vue'
+
+export default {
+    name: "CarouselOptions",
+    components: {
+        SeeResults,
+    },
+    setup() {
+        return {
+            slide: ref('style'),
+            lorem: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo.'
+        }
+    }
+}
+</script>
---
+++ Frontend/PokemonReviewAppFrontend/src/layouts/MainLayout.vue
@@ -0,0 +1,79 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          Pokemon Review App
+        </q-toolbar-title>
+
+        <div>v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header>
+          Some Links
+        </q-item-label>
+
+        <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+      <CarouselOptions></CarouselOptions>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+import CarouselOptions from 'components/CarouselOptions.vue'
+
+const linksList = [
+  {
+    title: 'Github',
+    caption: 'Github link to this project',
+    icon: 'code',
+    link: 'https://github.com/Emredinho61/pokemonReviewApp/'
+  },
+  {
+    title: 'LinkedIn',
+    caption: 'My LinkedIn profile',
+    icon: 'person',
+    link: 'https://linkedin.com/in/emre-bayazitoglu'
+  },
+  {
+    title: 'YouTube Tutorial',
+    caption: 'YouTube Tutorial to Backend',
+    icon: 'play_circle_filled',
+    link: 'https://www.youtube.com/playlist?list=PL82C6-O4XrHdiS10BLh23x71ve9mQCln0'
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink,
+    CarouselOptions
+  },
+
+  data() {
+    return {
+      linksList,
+      leftDrawerOpen: false
+    }
+  },
+
+  methods: {
+    toggleLeftDrawer() {
+      this.leftDrawerOpen = !this.leftDrawerOpen
+    }
+  }
+})
+</script>
